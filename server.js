cef296ae7f7d1311ff35aae4d00fa0843fcee675
System <none>
2017-07-10 11:26:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,34 +4,18 @@
 // init project
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+var MongoClient = mongodb.MongoClient;
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+var url = 'mongodb://localhost:27017/ds153392/url-shortener-microservice-db';
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
